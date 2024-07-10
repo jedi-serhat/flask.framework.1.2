1496558bec56aa0c834df400c27e47dd74de51c4
serhatseverneu <serhatneuchatel@gmail.com>
2024-07-10 17:01:59 +0200

    second

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,21 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return "Hello  Serhat. Ne yapacaz bu Kodlari"
+
+@app.route("/second")
+def head2():
+    return "Yalan, baskasi yalan, olumden baskasi yalan"
+
+@app.route("/third")
+def head3():
+    return "Yeter artik"
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"Ne istersen onu basarim {id}"
 
 if __name__ == '__main__':
 
